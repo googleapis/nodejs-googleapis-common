59bd75dcdad9cd3f7a8db7f5352c8c6f83234ea1
Jonathan Lui <jonathanlui@google.com>
2018-10-12 15:00:59 -0700

    build: bring in Kokoro cfgs (#13)

--- .kokoro/test.sh
+++ .kokoro/test.sh
@@ -1,20 +1,26 @@
-# Copyright 2018, Google, LLC.
+#!/bin/bash
+
+# Copyright 2018 Google LLC
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import synthtool as s
-import synthtool.gcp as gcp
-import logging
-import subprocess
+set -xeo pipefail
 
-logging.basicConfig(level=logging.DEBUG)
-common_templates = gcp.CommonTemplates()
-templates = common_templates.node_library()
-s.copy(templates)
+export NPM_CONFIG_PREFIX=/home/node/.npm-global
+
+cd $(dirname $0)/..
+
+npm install
+npm test
+
+bash $KOKORO_GFILE_DIR/codecov.sh
