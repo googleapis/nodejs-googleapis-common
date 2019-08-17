8734154a219c3a2bf865dc830bc95fbb5a640794
Benjamin E. Coe <bencoe@google.com>
2019-08-17 13:51:28 -0700

    refactor: process.versions.node does not have v prefix (#161)

--- src/apirequest.ts
+++ src/apirequest.ts
@@ -1,15 +1,18 @@
-// Copyright 2014-2016, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**
+ * Copyright 2016 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import {GaxiosPromise, Headers} from 'gaxios';
 import {DefaultTransporter, OAuth2Client} from 'google-auth-library';
@@ -314,7 +317,8 @@ function populateAPIHeader(headers: Headers) {
   // populating the gl-web header (web support should also be added to
   // google-auth-library-nodejs).
   if (!isBrowser()) {
-    const nodeVersion = process.version.replace(/^v/, '');
-    headers['x-goog-api-client'] = `gdcl/${pkg.version} gl-node/${nodeVersion}`;
+    headers[
+      'x-goog-api-client'
+    ] = `gdcl/${pkg.version} gl-node/${process.versions.node}`;
   }
 }
