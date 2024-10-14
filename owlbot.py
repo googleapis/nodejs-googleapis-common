2a961f8fa5069331cdbf12ad07d39aceb89f230d
Sofia Leon <sofialeon@google.com>
2024-10-14 16:37:48 -0700

    fix owlbot and system test

--- owlbot.py
+++ owlbot.py
@@ -1,6 +1,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import synthtool as s
+import synthtool.gcp as gcp
 import synthtool.languages.node as node
+import logging
 
-node.owlbot_main(templates_excludes=["LICENSE", "README.md", ".github/ISSUE_TEMPLATE", ".github/scripts", ".kokoro", ".github/workflows/issues-no-repro.yaml", ".jsdoc.js"])
+logging.basicConfig(level=logging.DEBUG)
+
+common_templates = gcp.CommonTemplates()
+templates = common_templates.node_library()
+s.copy(excludes=["LICENSE", "README.md", ".github/ISSUE_TEMPLATE", ".github/scripts", ".kokoro", ".github/workflows/issues-no-repro.yaml", ".jsdoc.js"])
